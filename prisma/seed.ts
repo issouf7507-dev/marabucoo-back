02c78c4d978aa8888715c1fbe504c2428a76cc9f
Ouattara Bitcheresse Issouf <issouf@192.168.1.6>
2026-06-16 17:59:06 +0000

    fe

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,7 @@ import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma";
 import bcrypt from "bcryptjs";
+// .
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
